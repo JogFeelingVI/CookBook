24254c35bb21cec7551a8669090e10b6a94d9f33
JogfeeingVI <lifelse@outlook.com>
2023-03-19 21:33:21 +0800

    updatemups

--- mupvalue.py
+++ mupvalue.py
@@ -1,18 +1,35 @@
-from multiprocessing import Process, Value
+import multiprocessing, itertools
 
-def func(counter):
-    with counter.get_lock():
-        counter.value += 1
+
+class mups:
+    @staticmethod
+    def ites():
+        lisx = itertools.combinations((x for x in range(1, 34)), 6)
+        return lisx
+
+    @staticmethod
+    def funx(p: tuple) -> float:
+        z = 1
+        for pi in p:
+            z = z * pi
+        return z**(1 / p.__len__())
+
+    @staticmethod
+    def caule_p(p: tuple) -> str:
+        L = p.__len__()
+        fstr = '{:02}'
+        strx = ' '.join(map(fstr.format, p))
+        pfunx = mups.funx(p)
+        if 13 <= pfunx <= 14:
+            strx = f'{strx} / {mups.funx(p):.0f}'
+        else:
+            strx = 'NONE'
+        return strx
+
 
 if __name__ == '__main__':
-    counter = Value('i', 0)
-    processes = [Process(target=func, args=(counter,)) for i in range(100)]
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
-    print(f'Progress: {counter.value}/10')
-
-
-
-
+    main_mps = mups()
+    with multiprocessing.Pool(processes=8) as pls:
+        rx = pls.imap(main_mps.caule_p, main_mps.ites(), chunksize=10000)
+        for ir in rx:
+            print(ir)
