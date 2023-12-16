b2ac58e6268d4fbac21cfba692704a7bf0c13a9d
JogfeeingVI <lifelse@outlook.com>
2023-12-16 23:01:06 +0800

    update thread 3 funcs

--- bins.py
+++ bins.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: JogFeelingVI
+# @Date:   2021-11-21 21:12:49
+# @Last Modified by:   JogFeelingVI
+# @Last Modified time: 2023-12-16 16:03:40
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date	: 2019/8/29 11:10 下午
@@ -5,35 +10,32 @@
 # @Link	: blog.sina.com.cn/lifelse
 # @Name	: bins
 class nbin:
-	num, bins = 1, 0b1
-	
-	def __init__(self, Ns: int = 1):
-		self.num = Ns
-		self.bins = bin(self.num)
-	
-	def Bins(self) -> str:
-		return self.bins
-	
-	def BinsX(self, bit:int=7) -> str:
-		sr = '{{:0>{}}}'.format(bit)
-		sr = sr.format(self.bins.replace('0b', ''))
-		return sr
-		
-	def Str(self) -> str:
-		return 'Nu {:>4} bin {}'.format(self.num, self.bins)
-	
-	def Dict(self) -> dict:
-		return {'num': self.num, 'bins': self.bins}
-	
-	def List(self) -> list:
-		return [self.num, self.bins]
+    num, bins = 1, '0b1'
+
+    def __init__(self, Ns: int = 1):
+        self.num = Ns
+        self.bins: str = bin(self.num)
+
+    def BinsX(self, bit: int = 7) -> str:
+        sr = '{{:0>{}}}'.format(bit)
+        sr = sr.format(self.bins.replace('0b', ''))
+        return sr
+
+    def Str(self) -> str:
+        return 'Nu {:>4} bin {}'.format(self.num, self.bins)
+
+    def Dict(self) -> dict:
+        return {'num': self.num, 'bins': self.bins}
+
+    def List(self) -> list:
+        return [self.num, self.bins]
 
 
 def loads():
-	for x in range(1, 101):
-		nv = nbin(x)
-		print('{:>4} is {}'.format(x, ' '.join(nv.BinsX(7))))
+    for x in range(1, 101):
+        nv = nbin(x)
+        print('{:>4} is {}'.format(x, ' '.join(nv.BinsX(7))))
 
 
 if __name__ == '__main__':
-	loads()
+    loads()
---
+++ thread_23.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+# @Author: JogFeelingVI
+# @Date:   2023-12-16 16:04:18
+# @Last Modified by:   JogFeelingVI
+# @Last Modified time: 2023-12-16 22:54:15
+
+from io import TextIOWrapper
+import urllib.request, time, threading, re, queue
+
+urls = [
+    'http://www.python.org/', 'https://www.baidu.com',
+    'https://www.google.com', 'https://sspai.com', 'https://www.appinn.com'
+]
+
+
+def do_gethood(uq: queue.Queue, pq: queue.Queue):
+    while True:
+        url = uq.get()
+        html = gethood(url=url)
+        pq.put((url, html))
+        print(
+            f'U {threading.current_thread().name} UQ {uq.qsize()} PQ {pq.qsize()}'
+        )
+        time.sleep(3)
+        if uq.empty():
+            break
+    print(f'UQ {threading.current_thread().name} is END {time.time()}')
+
+
+def do_pares(pq: queue.Queue, fp: TextIOWrapper):
+    stime = []
+    count_zone = 0
+    while True:
+        if pq.empty():
+            stime.append(time.time())
+            sts = stime[-2:]
+            if sts.__len__() == 2:
+                zone = sts[1] - sts[0]
+                if zone < 1.01:
+                    count_zone += 1
+                    print(f'{threading.current_thread().name} is {count_zone}')
+                if count_zone > 5:
+                    break
+            time.sleep(1)
+            continue
+        count_zone = 0
+        stime.clear()
+        u, h = pq.get()
+        fp.write(f'link for {u} {time.time()}\n{"="*32}\n')
+        href = pares(h)
+        if href != None:
+            for link in href:
+                fp.write(f'{link}\n')
+            print(
+                f'P {threading.current_thread().name} PQ {pq.qsize()} URL {u} link {len(href)}'
+            )
+        time.sleep(3)
+
+
+def do_wallking():
+    uq = queue.Queue()
+    pq = queue.Queue()
+    for u in urls:
+        uq.put(u)
+
+    for idx in range(3):
+        t = threading.Thread(target=do_gethood,
+                             args=(uq, pq),
+                             name=f'thr-{idx}')
+        t.start()
+    fp = open('02_links.log', '+w')
+    for idx in range(2):
+        t = threading.Thread(target=do_pares, args=(pq, fp), name=f'thw-{idx}')
+        t.start()
+
+
+def gethood(url: str):
+    with urllib.request.urlopen(url=url) as fr:
+        return fr.read().decode('utf-8')
+
+
+def pares(html: str):
+    '''
+    <a href="https://www.appinn.com/chatgpt-guides/">ChatGPT 指南</a>
+    '''
+    href_link = []
+    link = re.compile(r'<a(.*)a>')
+    href = re.compile(r'(http|https)://([a-zA-Z0-9:./#-?]+)')
+    if html == None or html == '':
+        return None
+    link_m = link.findall(html)
+    for lm in link_m:
+        href_m = href.findall(lm)
+        for hm in href_m:
+            h, m = hm
+            href_link.append(f'{h}://{m}')
+    print(f'Find href counts {href_link.__len__()}')
+    return href_link
+
+
+def mu_thread():
+    print('wallking in thread')
+    stime = time.time()
+    thr = []
+    for u in urls:
+        thr.append(threading.Thread(target=gethood, args=(u, )))
+    for t in thr:
+        t.start()
+    for t in thr:
+        t.join()
+    print(f'gethood wallking time use thread {time.time() - stime:.4f}')
+
+
+def code():
+    print("Hello, World!")
+    stime = time.time()
+    for u in urls:
+        html = gethood(url=u)
+        pares(html)
+    print(f'gethood wallking time {time.time() - stime:.4f}')
+
+
+if __name__ == "__main__":
+    code()
+    #mu_thread()
+    do_wallking()
